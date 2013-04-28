58d21a8215de2baf890ecb0b6a7103eb218a2c13
Jim Lawton <jim.lawton@gmail.com>
2013-04-28 23:21:54 +0100

    Rearrange. Looks unwieldy, but this way I can more exactly control what gets put into the lines. The problem is 42 is the nicest font, and at that size, I need to drop out the prefix stuff in line 1 for the quarter/twenty/twenty-five times.

--- src/fuzzy_time.c
+++ src/fuzzy_time.c
@@ -20,69 +20,84 @@ static const char* const HOURS[] = {
 // Time      line1     line2     line3
 // ----      --------  --------  --------
 // 5:00      it's      five      o'clock
-// 5:01-03             after     five
+// 5:01-03   gone      five      o'clock
 // 5:04      nearly    five past five
 // 5:05                five past five
 // 5:06-08   after     five past five
 // 5:09      nearly    ten past  five
 // 5:10                ten past  five
 // ..
-// 5:44      nearly    quarter to six
-// 5:45                quarter to six
-// 5:46-48   after     quarter to six
+// 5:44      quarter   to        six
+// 5:45      quarter   to        six
+// 5:46-48   quarter   to        six
 // 5:49      nearly    ten to    six
 // 5:50                ten to    six
 
 void fuzzy_time(int hours, int minutes, char* line1, char* line2, char* line3) {
-    strcpy(line1, "");
-    strcpy(line2, "");
-    strcpy(line3, "");
 
-    if (minutes == 0) {
-        strcat(line1, "it's");
-    } else if (minutes % 5 >= 1 && minutes % 5 < 4) {
-        strcat(line1, "after");
-    } else if (minutes % 5 == 4) {
-        strcat(line1, "nearly");
+    if (minutes % 5 >= 1 && minutes % 5 < 4) {
+        strcpy(line1, "gone");
+    }
+
+    if (minutes % 5 == 4) {
+        strcpy(line1, "nearly");
+    }
+
+    if (minutes >= 34) {
+        hours += 1;
     }
 
     if (hours > 12)
         hours -= 12;
 
-    if (minutes == 59 || (minutes >= 0 && minutes < 4)) {
-        if (minutes == 59) {
-            hours += 1;
-            if (hours > 12)
-                hours -= 12;
-        }
-        strcat(line2, HOURS[hours]);
-    } else {
-        if ((minutes >= 4 && minutes < 9) || (minutes >= 54 && minutes < 59)) {
-            strcat(line2, "five");
-        } else if ((minutes >= 9 && minutes < 14) || (minutes >= 49 && minutes < 54)) {
-            strcat(line2, "ten");
-        } else if ((minutes >= 14 && minutes < 19) || (minutes >= 44 && minutes < 49)) {
-            strcat(line2, "quarter");
-        } else if ((minutes >= 19 && minutes < 24) || (minutes >= 39 && minutes < 44)) {
-            strcat(line2, "twenty");
-        } else if ((minutes >= 24 && minutes < 29) || (minutes >= 34 && minutes < 39)) {
-            strcat(line2, "twenty five");
-        } else if (minutes >= 29 && minutes < 34) {
-            strcat(line2, "half");
-        }
-        if (minutes < 34) {
-            strcat(line2, " past");
-        } else {
-            hours += 1;
-            if (hours > 12)
-                hours -= 12;
-            strcat(line2, " to");
-        }
-    }
-
-    if (minutes == 59 || (minutes >= 0 && minutes < 4)) {
-        strcat(line3, "o'clock");
-    } else {
-        strcat(line3, HOURS[hours]);
+    if (minutes == 0) {
+        strcpy(line1, "it's");
+        strcpy(line2, HOURS[hours]);
+        strcpy(line3, "o'clock");
+    } else if (minutes >= 1 && minutes < 4) {
+        strcpy(line2, HOURS[hours]);
+        strcpy(line3, "o'clock");
+    } else if (minutes >= 4 && minutes < 9) {
+        strcpy(line2, "five past");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 9 && minutes < 14) {
+        strcpy(line2, "ten past");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 14 && minutes < 19) {
+        strcpy(line1, "quarter");
+        strcpy(line2, "past");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 19 && minutes < 24) {
+        strcpy(line1, "twenty");
+        strcpy(line2, "past");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 24 && minutes < 29) {
+        strcpy(line1, "twenty");
+        strcpy(line2, "five past");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 29 && minutes < 34) {
+        strcpy(line2, "half past");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 34 && minutes < 39) {
+        strcpy(line1, "twenty");
+        strcpy(line2, "five to");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 39 && minutes < 44) {
+        strcpy(line1, "twenty");
+        strcpy(line2, "to");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 44 && minutes < 49) {
+        strcpy(line1, "quarter");
+        strcpy(line2, "to");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 49 && minutes < 54) {
+        strcpy(line2, "ten to");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes >= 54 && minutes < 59) {
+        strcpy(line2, "five to");
+        strcpy(line3, HOURS[hours]);
+    } else if (minutes == 59) {
+        strcpy(line2, HOURS[hours]);
+        strcpy(line3, "o'clock");
     }
 }
