f108e6e07ac08a88860f2dd0fea2fd39257151d6
Jim Lawton <jim.lawton@gmail.com>
2013-04-29 14:02:12 +0100

    More tweaking of the different time bands.

--- src/fuzzy_time.c
+++ src/fuzzy_time.c
@@ -50,51 +50,169 @@ void fuzzy_time(int hours, int minutes, char* line1, char* line2, char* line3) {
     if (hours > 12)
         hours -= 12;
 
-    if (minutes == 0) {
+    switch (minutes) {
+    case 0:
         strcpy(line1, "it's");
         strcpy(line2, HOURS[hours]);
         strcpy(line3, "o'clock");
-    } else if (minutes >= 1 && minutes < 4) {
+        break;
+    case 1:
+    case 2:
+    case 3:
         strcpy(line2, HOURS[hours]);
         strcpy(line3, "o'clock");
-    } else if (minutes >= 4 && minutes < 9) {
+        break;
+    case 4:
         strcpy(line2, "5 past");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 9 && minutes < 14) {
+        break;
+    case 5:
+        strcpy(line1, "five");
+        strcpy(line2, "past");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 6:
+    case 7:
+    case 8:
+        strcpy(line2, "5 past");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 9:
         strcpy(line2, "10 past");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 14 && minutes < 19) {
+        break;
+    case 10:
+        strcpy(line1, "ten");
+        strcpy(line2, "past");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 11:
+    case 12:
+    case 13:
+        strcpy(line2, "10 past");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
         strcpy(line1, "quarter");
         strcpy(line2, "past");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 19 && minutes < 24) {
+        break;
+    case 19:
         strcpy(line2, "20 past");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 24 && minutes < 29) {
+        break;
+    case 20:
+        strcpy(line1, "twenty");
+        strcpy(line2, "past");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 21:
+    case 22:
+    case 23:
+        strcpy(line2, "20 past");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 24:
         strcpy(line2, "25 past");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 29 && minutes < 34) {
+        break;
+    case 25:
+        strcpy(line1, "25");
+        strcpy(line2, "past");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 26:
+    case 27:
+    case 28:
+        strcpy(line2, "25 past");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 29:
+    case 30:
+    case 31:
+    case 32:
+    case 33:
         strcpy(line1, "half");
         strcpy(line2, "past");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 34 && minutes < 39) {
+        break;
+    case 34:
         strcpy(line2, "25 to");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 39 && minutes < 44) {
+        break;
+    case 35:
+        strcpy(line1, "25");
+        strcpy(line2, "to");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 36:
+    case 37:
+    case 38:
+        strcpy(line1, "25");
+        strcpy(line2, "to");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 39:
         strcpy(line2, "20 to");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 44 && minutes < 49) {
+        break;
+    case 40:
+        strcpy(line1, "twenty");
+        strcpy(line2, "to");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 41:
+    case 42:
+    case 43:
+        strcpy(line2, "20 to");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 44:
+    case 45:
+    case 46:
+    case 47:
+    case 48:
         strcpy(line1, "quarter");
         strcpy(line2, "to");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 49 && minutes < 54) {
+        break;
+    case 49:
         strcpy(line2, "10 to");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes >= 54 && minutes < 59) {
+        break;
+    case 50:
+        strcpy(line1, "ten");
+        strcpy(line2, "to");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 51:
+    case 52:
+    case 53:
+        strcpy(line2, "10 to");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 54:
         strcpy(line2, "5 to");
         strcpy(line3, HOURS[hours]);
-    } else if (minutes == 59) {
+        break;
+    case 55:
+        strcpy(line1, "five");
+        strcpy(line2, "to");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 56:
+    case 57:
+    case 58:
+        strcpy(line2, "5 to");
+        strcpy(line3, HOURS[hours]);
+        break;
+    case 59:
         strcpy(line2, HOURS[hours]);
         strcpy(line3, "o'clock");
+        break;
     }
 }
